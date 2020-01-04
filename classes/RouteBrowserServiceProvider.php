d7d3623daca3e8f99a000da2deba884d9cb8af0f
Dave James Miller <dave@davejamesmiller.com>
2020-01-04 19:56:40 +0000

    Move service provider calls to match latest docs

--- classes/RouteBrowserServiceProvider.php
+++ classes/RouteBrowserServiceProvider.php
@@ -11,17 +11,21 @@ class RouteBrowserServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        // Load the default config values
         $this->mergeConfigFrom(__DIR__ . '/../config/route-browser.php', 'route-browser');
     }
 
     public function boot(): void
     {
+        // Register routes
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
+
+        // Register 'route-browser::' view namespace
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'route-browser');
+
+        // Publish the config/route-browser.php file
         $this->publishes([
             __DIR__ . '/../config/route-browser.php' => config_path('route-browser.php'),
         ], 'route-browser-config');
-
-        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'route-browser');
-
-        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
     }
 }
