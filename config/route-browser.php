8a1e3cf590b96899886060c74533f893fb70a339
Dave Miller <dave@alberon.co.uk>
2020-01-30 14:16:53 +0000

    Exclude ignition-vendor, horizon, telescope & tinker

--- config/route-browser.php
+++ config/route-browser.php
@@ -38,6 +38,10 @@ return [
     'exclude' => [
         '/_debugbar/*', // https://github.com/barryvdh/laravel-debugbar
         '/_ignition/*', // https://github.com/facade/ignition
+        '/horizon', '/horizon/*', // https://laravel.com/docs/horizon
+        '/ignition-vendor/*', // https://flareapp.io/docs/ignition-for-laravel/third-party-extensions
+        '/telescope', '/telescope/*', // https://laravel.com/docs/telescope
+        '/tinker', // https://github.com/spatie/laravel-web-tinker
     ],
 
     // Exclude Route Browser's own routes?
